53ad707ee303574ac22eccab1defb26b3168147f
jbenet <jbenet@cs.stanford.edu>
2011-09-01 17:30:33 -0700

    bumped version up

--- bsonnetwork/__init__.py
+++ bsonnetwork/__init__.py
@@ -3,7 +3,7 @@ BsonNetwork Package
 '''
 
 __author__ = 'Juan Batiz-Benet'
-__version__ = '0.3.5'
+__version__ = '0.3.6'
 # don't forget to change it in setup.py
 
 from network import BsonNetworkFactory, BsonNetworkProtocol
